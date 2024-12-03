9f657725a9afbfb33a2972d7319bb10489ab28e8
Youness Elalouani <younesselalouani1995@gmail.com>
2024-12-03 11:37:32 +0100

    push the lips website

---
+++ main.js
@@ -0,0 +1,83 @@
+const navbar = document.querySelector('.navbar__fixed ');
+const menu = document.querySelector('.icone__menu');
+const menuIcon = document.querySelector('.bx__menu');
+const lineOne = document.querySelector('.line__one');
+const lineThree = document.querySelector('.line__three');
+
+const listItems = document.querySelectorAll('.list__lien ul li a');
+
+const listCoor = document.querySelector('.list__coor');
+const imgRotate = document.querySelector('.rotate__fixed img');
+
+menu.addEventListener('click', () => {
+    navbarProp()
+
+})
+
+navbar.addEventListener('click', (e) => {
+    navbarProp()
+    if (e.target.tagName === "SPAN") {
+        navbar.classList.remove("active")
+    }
+
+})
+
+function navbarProp() {
+    navbar.classList.toggle('active');
+    lineOne.classList.toggle('disabled');
+    lineThree.classList.toggle('disabled');
+    images[0].classList.toggle('active')
+
+    listItems.forEach(item => {
+        item.classList.toggle('lien__enabled')
+    })
+
+    listCoor.classList.toggle('coor__enabled')
+    imgRotate.classList.toggle('enabled')
+
+    if (navbar.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = 'visible';
+    }
+}
+
+
+
+// ==========================DOMLOADED
+const titleLips = document.querySelector('.lips__info h1');
+const paraLips = document.querySelector('.lips__info p');
+
+const lipsImg = document.querySelector('.lips__img');
+const imgLips = document.querySelector('.lips__img img')
+const cercle = document.querySelector('.lips__img .enable')
+const paraTwoLips = document.querySelector('.lips__content-two p')
+const categLips = document.querySelector('.lips__categories ')
+
+
+const navbarTop = document.querySelector('.header .navbar')
+window.addEventListener('DOMContentLoaded', () => {
+    navbarTop.classList.add('onload');
+    // for (let i = 0; i < myVar.length; i++) {
+    //     myVar[i].classList.add('enabled')
+    // }
+    const lipsInfo = document.querySelector(".lips__info")
+    lipsInfo.classList.add('line')
+})
+
+
+/*==============IMAGESNAVBAR===============*/
+const list = document.querySelectorAll('.list__lien ul li a');
+
+const images = document.querySelectorAll('.images__navbar img');
+for (let i = 1; i < images.length; i++) {
+    list[i].addEventListener('mouseenter', () => {
+        images[i].classList.add('active')
+    })
+
+    list[i].addEventListener('mouseleave', () => {
+        images[i].classList.remove('active')
+    })
+}
+
+
---
+++ scrollAnimationCat.js
@@ -0,0 +1,121 @@
+const colorSection = document.getElementById('colour__care');
+const itemsColor = document.querySelectorAll('.color__item')
+const cat = document.querySelector('.color__categories')
+
+const intro = document.querySelector('.colour__care-content')
+
+const controller = new ScrollMagic.Controller();
+
+
+
+
+
+if (window.innerWidth >= 1000) {
+
+    let parallaxTl = new TimelineMax();
+    parallaxTl
+        .to(itemsColor, { x: "-1000", duration: 4, ease: Linear.easeNone });
+    const scene = new ScrollMagic.Scene({
+        duration: 1500,
+        triggerElement: colorSection,
+        triggerHook: 0
+    })
+        // .addIndicators()
+        .setPin(colorSection)
+        .setTween(parallaxTl)
+        .addTo(controller)
+}
+
+
+
+
+itemsColor.forEach(item => {
+
+
+    const hover = gsap.to(item, {
+        y: 10,
+        duration: .001,
+        paused: true,
+        ease: "power1.inOut"
+    })
+
+    item.addEventListener('mouseenter', () => {
+        hover.play()
+    })
+
+    item.addEventListener('mouseleave', () => {
+        hover.reverse()
+    })
+})
+
+
+
+
+
+const sceneOne = new ScrollMagic.Scene({
+    triggerElement: '#about__content',
+    triggerHook: 0.6,
+    reverse: false
+})
+    .setClassToggle('#about__content', 'gsap_x')
+    .addTo(controller)
+
+
+const sceneTwo = new ScrollMagic.Scene({
+    triggerElement: '#colour__care',
+    triggerHook: 0.6,
+    reverse: false
+})
+    .setClassToggle('#colour__care', 'gsap_x')
+    .addTo(controller)
+
+
+
+const sceneThree = new ScrollMagic.Scene({
+    triggerElement: '.footer__one',
+    triggerHook: 0.6,
+    reverse: false
+})
+    .setClassToggle('.footer__one-content', 'gsap_y')
+    .addTo(controller)
+
+
+const sceneFoor = new ScrollMagic.Scene({
+    triggerElement: '.footer__two',
+    triggerHook: 1,
+    reverse: false
+})
+    .setClassToggle('.footer__two', 'gsap_y')
+    .addTo(controller)
+
+
+const sceneFive = new ScrollMagic.Scene({
+    triggerElement: '.lips__content-two',
+    triggerHook: 1,
+    reverse: false
+})
+    .setTween(TweenMax.from('.lips__content-two p', { x: 2000, opacity: 0, duration: 1, ease: Back.easeOut }))
+    .addTo(controller)
+
+
+const sceneSix = new ScrollMagic.Scene({
+    triggerElement: '.lips__content-two',
+    triggerHook: 1,
+    reverse: false
+})
+    .setTween(TweenMax.from('.lips__categories', { y: 300, opacity: 0, duration: 2 }))
+    .addTo(controller)
+
+
+const timeline = gsap.timeline({ duration: .5 })
+
+timeline
+    .fromTo('.promo__dektop', { opacity: 0, y: -100 }, { opacity: 1, y: 0 })
+    .from(['.lips__info h1', '.lips__info p'], { opacity: 0, y: -70, stagger: .5 })
+    .from('.lips__img', { y: -100, opacity: 0 })
+
+
+
+
+
+
